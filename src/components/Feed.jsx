9a7a6ff358a878de935a7a3e954538208efacb5e
Parag3195 <parag.shinde407@gmail.com>
2022-12-21 13:20:01 +0530

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,23 @@
+import { Box, Stack, Skeleton } from "@mui/material";
+import React, { useState } from "react";
+import { Post } from "./Post";
+
+const Feed = () => {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, [3000]);
+
+  return (
+     <Box  flex={4} p={2}>
+       <Post name="Parag" img="https://images.pexels.com/photos/3473569/pexels-photo-3473569.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+       <Post name="Utkarsha" img="https://images.pexels.com/photos/4562685/pexels-photo-4562685.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+       <Post name="Bhushan" img="https://images.pexels.com/photos/1886487/pexels-photo-1886487.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+
+    
+        </Box>
+  );
+};
+
+export default Feed;
